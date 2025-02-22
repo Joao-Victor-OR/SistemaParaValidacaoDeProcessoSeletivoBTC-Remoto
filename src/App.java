44d1f3e23352f9c8cae211c56217f59068f4ae58
Joao Victor Oliveira Rosa <joao.rosa@estudante.ufla.br>
2025-02-22 10:26:14 -0300

    Avaliando candidatos com base no salário

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Processo Seletivo!");
+        analisarCandidato(1600);
+        analisarCandidato(2400);
+        analisarCandidato(2000);
+
+    }
+    public static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.00;
+        if(salarioBase > salarioPretendido){
+            System.out.println("Ligar para candidato");
+        }else if(salarioBase == salarioPretendido){
+            System.out.println("Ligar para o candidato com contra proposta");
+        }
+        else{
+            System.out.println("Aguardando Resultado de demais candidatos");
+        }
     }
 }

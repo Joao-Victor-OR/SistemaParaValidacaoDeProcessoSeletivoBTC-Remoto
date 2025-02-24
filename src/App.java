e544973369917d8d1ad55744cf39a9373f089211
Joao Victor Oliveira Rosa <joao.rosa@estudante.ufla.br>
2025-02-24 08:38:14 -0300

    Simulando ligações para os candidatos

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class App {
@@ -11,7 +12,38 @@ public class App {
         /* 
         selecaoCandidatos();
         */
+        /*
         imprimirSelecionados();
+        */
+        String[] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto"};
+        for(String candidato: candidatos){
+            entrandoEmContato(candidato);
+        }
+
+    }
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do{
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando){
+                tentativasRealizadas++;
+            }else{
+                System.out.println("Contato Realizado Com Sucesso");
+            }
+
+        }while(continuarTentando && tentativasRealizadas < 3);
+        if(atendeu){
+            System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + " Tentativa");
+        }else{
+            System.out.println("Não conseguimos contato com " + candidato + ", numero máximo de tentativas " + tentativasRealizadas + " Realizada");
+        }
+    }
+
+    static boolean atender(){
+        return new Random().nextInt(3) == 1;
     }
     static void imprimirSelecionados(){
         String[] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto"};
